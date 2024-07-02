c0b068aa9535e132eac9b5316271ad005d016a52
Steven Gubkin <stevengubkin@Steve-2024-Macbook.local>
2024-07-02 08:29:37 -0400

    Added pyjokes to environment.yml and updated secret code.  Since pyjokes would rarely be installed, this should catch people who (intentionally or not) circumvent the conda env.

--- pressure_testing.py
+++ pressure_testing.py
@@ -2,11 +2,16 @@ import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import sklearn
+import pyjokes
 
 from sklearn.linear_model import LinearRegression
 
 def secret_code():
-    z = int(np.__version__.split('.')[0]) + 10*int(np.__version__.split('.')[0]) + 100*int(mpl.__version__.split('.')[0]) + 1000*int(sklearn.__version__.split('.')[0])
+    z = int(np.__version__.split('.')[0]) 
+    z += 10*int(np.__version__.split('.')[0]) 
+    z += 100*int(mpl.__version__.split('.')[0])
+    z += 1000*int(sklearn.__version__.split('.')[0])
+    z += 10000*int(pyjokes.__version__.split('.')[1])
     X = np.loadtxt("X.txt", delimiter=",")
     lr = LinearRegression()
     lr.fit(X[:,:2],X[:,2])
